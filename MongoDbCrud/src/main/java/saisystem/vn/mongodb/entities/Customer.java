a4ddc47558bb386b006777c929e76f6443371e73
TungVQ <tungvu.peony@gmail.com>
2019-03-18 18:16:43 +0700

    + Them gioi tinh cho customer

--- MongoDbCrud/src/main/java/saisystem/vn/mongodb/entities/Customer.java
+++ MongoDbCrud/src/main/java/saisystem/vn/mongodb/entities/Customer.java
@@ -1,33 +1,58 @@
 package saisystem.vn.mongodb.entities;
 
+import java.time.LocalDate;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "customer")
 public class Customer {
-	  @Id
-	  private String id;
-	  private String name;
-	  private String address;
-	  // getter - setter
+	@Id
+	private String id;
+	private String name;
+	private String address;
+	private Boolean gender; //true: Nam, false:Nu
+	private LocalDate birthDay;
+
+	// getter - setter
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	  
+
+	public Boolean getGender() {
+		return gender;
 	}
 
+	public void setGender(Boolean gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(LocalDate birthDay) {
+		this.birthDay = birthDay;
+	}
+
+}
