307026e11de8c141abf7fc6358810dc92bef2fda
thaipd <thaicnt50@gmail.com>
2019-03-21 13:54:11 +0700

    - remove

--- spring-boot-mybatis-example/src/main/resources/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users
-(
-    id serial NOT NULL,
-    name varchar(100) NOT NULL,
-    salary float(10) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
--- spring-boot-mybatis/README.md
+++
@@ -1,1 +0,0 @@
-# Spring Boot Mybatis Mysql Example
